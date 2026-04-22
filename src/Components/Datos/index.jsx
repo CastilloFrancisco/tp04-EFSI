259c3c3eaeccf47e48cb33a632e9556c40a0af07
Fran <francisco.castillo2704@gmail.com>
2026-04-22 00:46:35 -0300

    avance datos

--- src/Components/Datos/index.jsx
+++ src/Components/Datos/index.jsx
@@ -1,7 +1,27 @@
-import './datos.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import './datos.css';
 
-export default function Estadistica({ nombre }) {
-  return (
+export default function Datos({ nombre }) {
+  const [datos, setDatos] = useState(null);
+
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      try {
+        const res = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${nombre}`
+        );
+        setDatos(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    obtenerDatos();
+  }, [nombre]);
+
+  if (!datos) return <p>Cargando...</p>;
+    return (
     <>
       <Estadistica>{nombre}</Estadistica>
       <Estadistica texto={obtenerDatos(nombre).weight}/>
@@ -11,9 +31,4 @@ export default function Estadistica({ nombre }) {
     </>
   )
 }
-app.get('https://pokeapi.co/api/v2/pokemon/:nom', (req, res) => {            
 
-    let strNombre = req.params.nom
-   
-    return res.send(`Hola ${strNombre}👋`);
-})
